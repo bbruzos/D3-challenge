d95026500969ac707aab56993a14e9f6b89db46e
bbruzos <bbruzos@bu.edu>
2020-11-16 18:34:06 -0800

    reapproaching my code for chart

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,154 +1,55 @@
-// SVG Window
+// SVG window
 var svgWidth = 1000;
-var svgHeight = 500; 
+var svgHeight = 650;
 
+// SVG margins
 var margin = {
     top: 20,
     right: 40,
-    bottom: 80,
-    left: 100
+    bottom: 150,
+    left: 100;\
 };
 
-// Creating width/height of chart
-var width = svgWidth - margin.left -margin.right;
-var height = svgHeight - margin.top -margin.bottom;
+// Set chart height and width
+var width = svgWidth - margin.right - margin.left;
+var height = svgHeight - margin.top - margin.bottom;
 
-// SVG Wrapper
-var svg = d3.select("#scatter")
-.append("svg")
-.attr("width", svgWidth)
-.attr("height", svgHeight + 50);
+// Append div to scatter id
+var chart = d3.select('#scatter')
+  .append('div')
+  .classed('chart', true);
 
-// Add SVG group
-var chartGroup = svg.append("g")
-.attr("transform", 'translate(${margin.left}, ${margin.top})');
+// Append svg to chart 
+var svg = chart.append('svg')
+  .attr('width', svgWidth)
+  .attr('height', svgHeight);
 
-// Initial Params
-var CurrentXAxis = "poverty";
-var CurrentYAxis = "healthcare";
+// SVG group
+var chartGroup = svg.append('g')
+.attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// Creating X-scale
-function xScale(data, CurrentXAxis) {
+// x and y-axis initial params
+var chosenXAxis = 'poverty';
+var chosenYAxis = 'healthcare';
 
+// Func for x-scale to update upon label click
+function xScale(censusData, chosenXAxis) {
     var xLinearScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d[CurrentXAxis])-2,d3.max(data, d => d[CurrentXAxis])+2])
-      .range([height, 0]);
+    .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+    d3.max(censusData, d => d[chosenXAxis]) * 1.2])
+    .range([0, width]);
 
-    return yLinearScale
+return xLinearScale;
 }
 
-// Creating Y-scale
-function yScale(data, CurrentYAxis) {
-
+// Func for y-scale to update opun label click
+function yScale(censusData, chosenYAxis) {
     var yLinearScale = d3.scaleLinear()
-      .domain([d3.min(data, d => d[CurrentYAxis])-2,d3.max(data, d => d[CurrentYAxis])+2])
-      .range([height, 0]);
-  
-    return yLinearScale;
-  
+    .domain([d3.min(censusData, d => d[chosenYAxis]) * 0.8,
+    d3.max(censusData, d => d[chosenYAxis]) * 1.2])
+    .range([0, width]);
+
+return yLinearScale;
 }
 
-// Update circle group with a transition for X variable
-function renderXCircles(circleGroup, switchX, CurrentXAxis) {
-    circleGroup.transition()
-    .duration(1000)
-    .attr("cx", d => switchX(d[CurrentXAxis]))
-    .attr("dx", d => switchX[CurrentXAxis]);
 
-  return circleGroup;
-}
-
-// Update circle group with a transition for Y variable
-function renderYCircles(circleGroup, switchY, CurrentYAxis) {
-    circleGroup.transition()
-    .duration(1000)
-    .attr("cy", d => switchY(d[CurrentYAxis]))
-    .attr("dy", d => switchY[CurrentYAxis]);
-
-  return circleGroup;
-}
-
-// Retrieve Data 
-d3.csv("data.csv").then(function(data, err) {
-    if (err) throw err;
-data.forEach(d => {
-    d.poverty = +d.poverty;
-    d.povertyMoe = +d.povertyMoe;
-    d.age = +d.age;
-    d.ageMoe = +d.ageMoe;
-    d.income = +d.income;
-    d.incomeMoe = +d.incomeMoe;
-    d.healthcare = +d.healthcare;
-    d.healthcareLow = +d.healthcareLow;
-    d.healthcareHigh = +d.healthcareHigh;
-    d.obesity = +d.obesity;
-    d.obesityLow = +d.obesityLow;
-    d.obesityHigh = +d.obesityHigh;
-    d.smokes = +d.smokes;
-    d.smokesLow = +d.smokesLow;
-    d.smokesHigh = +d.smokesHigh;
-  });
-
-  // func for xLinearScale 
-  var xLinearScale = xScale(data, CurrentXAxis);
-  // func for yLinearScale
-  var yLinearScale = yScale(data, CurrentYAxis);
-
-  // Initialize axis func's
-  var bottomAxis = d3.axisBottom(xLinearScale);
-  var leftAxis = d3.axisLeft(yLinearScale);
-
-  // add x-axis & y-axis
-  var xAxis = chartGroup.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(bottomAxis);
-
-    var yAxis = chartGroup.append("g")
-    .call(leftAxis);
-
-  // Append circle groups
-  var circleGroup = chartGroup.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("g");
-
-  var circles = circleGroup.append("circle")
-    .attr("cx", d => xLinearScale(d[CurrentXAxis]))
-    .attr("cy", d => yLinearScale(d[CurrentYAxis]))
-    .attr("r", 15)
-    .classed('stateCirc', true);
-
-  // Insert circle text
-  var circleText = circleGroup.append("text")
-    .text(d => d.abbr)
-    .attr("dx", d => xLinearScale(d[CurrentXAxis]))
-    .attr("dy", d => yLinearScale(d[CurrentYAxis])+5) 
-    .classed('stateAcc', true);
-
-  // 3-axis label group for x-axis
-  var xLabels = chartGroup.append("g")
-    .attr("transform", `translate(${width / 2}, ${height + 20})`);
-
-  var PovertyLabel = xLabels.append("text")
-    .attr("x", 0)
-    .attr("y", 20)
-    .attr("value", "income")
-    .classed("active blue", true)
-    .text("In Poverty (%)");
-
-  // 3-axis label group for y-axis
-  var yLabels = chartGroup.append("g")
-  .attr("transform", "rotate(-90)")
-
-  var HealthcareLabel = yLabels.append("text")
-    .attr("y", -50)
-    .attr("x", -(height/2))
-    .attr("dy", "1em")
-    .attr("value", "healthcare") 
-    .classed("active green", true)
-    .text("Lacks Healthcare (%)");
-
-
-}).catch(function(error) {
-    console.log(error);
-});
